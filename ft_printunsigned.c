b6d7ee80d91edbaaf0e46ecf96500b47919204b3
Douglas Moreira Barros <domoreir@c2r6s2.42porto.com>
2024-08-11 22:50:34 +0100

    2024 11 08 2250 add

--- ft_printunsigned.c
+++ ft_printunsigned.c
@@ -14,11 +14,18 @@
 #include "printf.h"
 #include "libft.h"
 
-int	ft_printunsigned(va_arg(args, unsigned long long))
+int	ft_printunsigned(unsigned long long n))
 {
 	int	c;
 	
 	c = 0;
 	return (c);
+	
+	// PRINCIPAL - Ver se o n'umeor 'e 0, se for 0 printa 0.
+	// PRINCIPAL - Chamar uma funcao uitoa
+	// UITOA - igual ao itoa, muda s'o o tipo de variavel e nao tem que considerar resultados negativos.
+	// UITOA chama um num_len para calcular o tamanho do malloc a realizar (penso j'a tyer isso no itoa)
+	//chamar o printstr , pois j'a tem um array
+	
 }
 
